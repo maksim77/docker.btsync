162b1a618841d43489b44590fdc1b038888094f6
Максим <maksim77ster@gmail.com>
2015-01-24 00:36:29 +0300

    try argparse

--- startup.py
+++ startup.py
@@ -2,48 +2,55 @@
 import os
 import json
 import argparse
+desc = "Управляющий скрипт контейнера BtSync"
 
-def createParser ():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('name', nargs='?')
+parser = argparse.ArgumentParser(description=desc,
+                                 usage="startup.py add -k KEY",
+                                 formatter_class=argparse.RawTextHelpFormatter)
 
-    return parser
+list_commands = parser.add_argument_group('Список комманд')
+list_options = parser.add_argument_group('Опции')
 
-parser = createParser()
-namespace = parser.parse_args()
+list_commands.add_argument('cmd', choices=['add', 'delete'],
+                           metavar=" \nADD\tADD\nDEL\tDELETE")
+list_options.add_argument('-k', '--key', metavar="", help="Ключ")
+args = parser.parse_args()
 
 if os.path.exists('FLAG'):
     if os.environ.get('KEY'):
         CONFIG = {}
-        CONFIG ["device_name"] = "Docker BtSync"
-        CONFIG ["listening_port"] = 55555
-        CONFIG ["pid_file"] = "/opt/btsync/btsync.pid"
-        CONFIG ["use_upnp"] = "True"
-        CONFIG ["download_limit"] = 0
-        CONFIG ["upload_limit"] = 0
+        CONFIG["device_name"] = "Docker BtSync"
+        CONFIG["listening_port"] = 55555
+        CONFIG["pid_file"] = "/opt/btsync/btsync.pid"
+        CONFIG["use_upnp"] = "True"
+        CONFIG["download_limit"] = 0
+        CONFIG["upload_limit"] = 0
 
         FOLDERS = {}
-        FOLDERS ['secret'] = os.environ["KEY"]
+        FOLDERS['secret'] = os.environ["KEY"]
         if os.environ.get('FOLDER'):
-            FOLDERS ['dir'] = '/data/'+os.environ["FOLDER"]
+            FOLDERS['dir'] = '/data/'+os.environ["FOLDER"]
         else:
-            FOLDERS ['dir'] = '/data/bt_folder/'
-        FOLDERS ['use_relay_server'] = 'true'
-        FOLDERS ['use_tracker'] ='true'
-        FOLDERS ['use_sync_trash'] = 'true'
-        FOLDERS ['overwrite_changes'] = 'true'
+            FOLDERS['dir'] = '/data/bt_folder/'
+        FOLDERS['use_relay_server'] = 'true'
+        FOLDERS['use_tracker'] = 'true'
+        FOLDERS['use_sync_trash'] = 'true'
+        FOLDERS['overwrite_changes'] = 'true'
 
-        shared_folders=[]
-        shared_folders=[FOLDERS]
+        shared_folders = []
+        shared_folders = [FOLDERS]
 
-        CONFIG ["shared_folders"] = shared_folders
+        CONFIG["shared_folders"] = shared_folders
 
-        res_str = (json.dumps(CONFIG,indent=5))
+        res_str = (json.dumps(CONFIG, indent=5))
 
-        fopen = open('btsync.config','w')
+        fopen = open('btsync.config', 'w')
         for index in res_str:
             fopen.write(index)
         fopen.close()
-        print (json.dumps(CONFIG,indent=5))
+        print(json.dumps(CONFIG, indent=5))
     else:
         print("KEY not found")
+else:
+    print(args.cmd)
+    print(args.key)
