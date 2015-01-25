d16f2ccf69d0df0d808e4df80700dc584fc8d1cd
Максим <maksim77ster@gmail.com>
2015-01-25 23:43:01 +0300

    add switch logic

--- startup.py
+++ startup.py
@@ -1,23 +1,22 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
+
 import os
 import sys
 import json
 from argparse import RawTextHelpFormatter, ArgumentParser
 
 
-def CreateParser():
-    desc = "Управляющий скрипт контейнера BtSync"
-    parser = ArgumentParser(usage="startup.py add -k KEY",
-                            description=desc,
-                            formatter_class=RawTextHelpFormatter)
-
-    list_commands = parser.add_argument_group('Список комманд')
-    list_options = parser.add_argument_group('Опции')
-
-    list_commands.add_argument('cmd', choices=['add', 'delete'])
-    list_options.add_argument('-k', '--key', metavar="", help="Ключ")
-    return parser
+def folders(key, folder):
+    FOLDERS = {}
+    FOLDERS['secret'] = key
+    FOLDERS['dir'] = folder
+    FOLDERS['dir'] = '/data/bt_folder/'
+    FOLDERS['use_relay_server'] = 'true'
+    FOLDERS['use_tracker'] = 'true'
+    FOLDERS['use_sync_trash'] = 'true'
+    FOLDERS['overwrite_changes'] = 'true'
+    return FOLDERS
 
 
 def FirstRun():
@@ -28,23 +27,16 @@ def FirstRun():
     CONFIG["use_upnp"] = "True"
     CONFIG["download_limit"] = 0
     CONFIG["upload_limit"] = 0
-
-    FOLDERS = {}
-    FOLDERS['secret'] = os.environ["KEY"]
-    if os.environ.get('FOLDER'):
-        FOLDERS['dir'] = '/data/'+os.environ["FOLDER"]
-    else:
-        FOLDERS['dir'] = '/data/bt_folder/'
-    FOLDERS['use_relay_server'] = 'true'
-    FOLDERS['use_tracker'] = 'true'
-    FOLDERS['use_sync_trash'] = 'true'
-    FOLDERS['overwrite_changes'] = 'true'
-
     shared_folders = []
-    shared_folders = [FOLDERS]
-
+    if os.environ.get('FOLDER'):
+        shared_folders = [folders(os.environ["KEY"],
+                                  os.environ["FOLDER"])
+                          ]
+    else:
+        shared_folders = [folders(os.environ["KEY"],
+                                  "/data/bt_folder/")
+                          ]
     CONFIG["shared_folders"] = shared_folders
-
     res_str = (json.dumps(CONFIG, indent=5))
 
     fopen = open('btsync.config', 'w')
@@ -52,6 +44,20 @@ def FirstRun():
         fopen.write(index)
     fopen.close()
     print(json.dumps(CONFIG, indent=5))
+
+
+def CreateParser():
+    parser_description = "Управляющий скрипт контейнера BtSync"
+    parser = ArgumentParser(usage="startup.py add -k KEY",
+                            description=parser_description,
+                            formatter_class=RawTextHelpFormatter)
+
+    list_commands = parser.add_argument_group('Список комманд')
+    list_options = parser.add_argument_group('Опции')
+
+    list_commands.add_argument('cmd', choices=['add', 'delete'])
+    list_options.add_argument('-k', '--key', metavar="", help="Ключ")
+    return parser
 
 
 if os.path.exists('FLAG'):
@@ -64,3 +70,12 @@ if os.path.exists('FLAG'):
 
 parser = CreateParser()
 args = parser.parse_args()
+
+if args.cmd == "add":
+    if args.key:
+        key = args.key
+    else:
+        key = input("Введите ключ:")
+    print(key)
+elif args.cmd == "delete":
+    print(args.cmd)
